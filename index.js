894ce7053fbe0a79baf78a6e1dc663017a418399
April <a.d.cidoni@gmail.com>
2024-05-05 11:35:49 -0400

    phase-1-array-filter-m ethod-lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, name) {
+ return array.filter(item => item.toLowerCase() === name.toLowerCase())
+ }
+
+function fuzzyMatch(array, name) {
+   return array.filter(item => item.toLowerCase().startsWith(name.toLowerCase()))
+
+}
+function matchName(array, name ) {
+   return array.filter(array => array.name === name)
+}
